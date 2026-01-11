a46e6537c4c4564fa3129fbc9690982e0bd5b7c3
Indrajeet <indrajeet.hujare@gmail.com>
2026-01-11 17:14:31 +0530

    Added Unique Pointer

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "Calculator.h"
+#include <memory>
 
 int main()
 {
-    Calculator calc;
-    std::cout << "Addition: " << calc.Add(5, 3) << std::endl;
-    std::cout << "Subtraction: " << calc.Subtract(5, 3) << std::endl;
+    std::unique_ptr<Calculator> calc = std::make_unique<Calculator>();
+    std::cout << "Addition: " << calc->Add(5, 3) << std::endl;
+    std::cout << "Subtraction: " << calc->Subtract(5, 3) << std::endl;
     return 0;
 }
